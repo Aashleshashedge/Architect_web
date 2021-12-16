6b341cba6c31de44ba01aeb5e9890b60b30f7488
Aashlesha Shedge <aashleshashedge@gmail.com>
2021-12-16 23:23:55 +0530

    architect complete website

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 window.onload = () => {
     setTimeout(() => {
+        /**
+       css classes 
+       */
       document.querySelector("body").classList.add("display");
-    }, 4000);
+    }, 500);
   };
-  
+  /**
+       on click event listner
+       */
   document.querySelector(".hamburger-menu").addEventListener("click", () => {
+      /**
+       colour changes
+       */
     document.querySelector(".container").classList.toggle("change");
   });
   
-  document.querySelector(".scroll-btn").addEventListener("click", () => {
-    document.querySelector("html").style.scrollBehavior = "smooth";
-    setTimeout(() => {
-      document.querySelector("html").style.scrollBehavior = "unset";
-    }, 1000);
-  });
-
-
+  
   
